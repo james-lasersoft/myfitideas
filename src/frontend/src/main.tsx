cf71563b2f4bf572901e7ce101a4f4ea3aa8e1fc
james-lasersoft <37112079+james-lasersoft@users.noreply.github.com>
2026-08-01 19:03:41 -0500

    Load application-wide Brand v1.0 page styles

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles/brand-tokens.css'
 import './index.css'
 import './styles/brand-components.css'
+import './styles/all-pages-brand.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
