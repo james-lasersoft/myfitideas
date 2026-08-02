379b9e5bceb9a36f8fce397e165e0a5e2d108208
james-lasersoft <37112079+james-lasersoft@users.noreply.github.com>
2026-08-01 19:00:31 -0500

    Load Brand v1.0 component styling

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/brand-tokens.css'
 import './index.css'
+import './styles/brand-components.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
