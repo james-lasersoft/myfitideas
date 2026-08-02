de609a77ee6309184f4374c8916ffd7b0d941ce2
james-lasersoft <37112079+james-lasersoft@users.noreply.github.com>
2026-08-02 18:51:57 -0500

    Add workspace switcher to global controls

--- src/frontend/src/components/layout/GlobalControls.tsx
+++ src/frontend/src/components/layout/GlobalControls.tsx
@@ -1,11 +1,13 @@
 import LanguageSelector from "../LanguageSelector";
 import ThemeToggle from "../ThemeToggle";
+import WorkspaceSwitcher from "./WorkspaceSwitcher";
 import "./GlobalControls.css";
 
 export default function GlobalControls() {
   return (
     <div className="global-controls-bar">
       <div className="global-controls-group">
+        <WorkspaceSwitcher />
         <ThemeToggle />
         <LanguageSelector />
       </div>
